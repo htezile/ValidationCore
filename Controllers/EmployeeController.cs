fd47b250d60153d56c468de6ead1372a7273f5f1
htezile <evelin_elizethc@hotmail.com>
2023-04-11 15:52:03 +0200

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidationCore.Models;
+
+namespace ValidationCore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        static List<Employee> allEmployees = new List<Employee>();
+        public IActionResult AllEmployee()
+        {
+            return View(allEmployees);
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                allEmployees.Add(employee);
+                return RedirectToAction("AllEmployee");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ValidationCore.Models
+{
+    public class Employee
+    {
+    
+        public int Id { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression(@"^[^@]+@[^@]+\.[a-zA-Z]{2,}$", ErrorMessage="Invalid email format")]
+        public string Email { get; set; }
+        [Range(1000, 2500)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Salary { get; set; }
+    }
+}
